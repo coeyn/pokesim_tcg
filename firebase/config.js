8e1518dced02268c413be66a7f61fb6e5b28153b
coeyn <coeyn22@gmail.com>
2026-02-24 19:50:40 +0100

    config

--- firebase/config.js
+++ firebase/config.js
@@ -1,11 +1,21 @@
-// Fill with your Firebase Web app config from Firebase Console.
-// Keep this file out of public sharing if you don't want to expose project identifiers.
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAieng4284Yg6KEnMY30pSV5WRBkpAIJdo",
+  authDomain: "pokesimtcg.firebaseapp.com",
+  projectId: "pokesimtcg",
+  storageBucket: "pokesimtcg.firebasestorage.app",
+  messagingSenderId: "246061902483",
+  appId: "1:246061902483:web:502a6bd7cbd4123a693b66",
+  measurementId: "G-J3H7LC9ENB"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
